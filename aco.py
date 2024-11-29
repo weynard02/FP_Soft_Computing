89cc61e10e448287a553d988097c3372f77328c2
weynard02 <samsicoweynard@gmail.com>
2024-11-29 08:41:57 +0700

    feat: aso using dataset input

--- aco.py
+++ aco.py
@@ -90,22 +90,30 @@ class SolveTSPUsingACO:
         plt.ylabel('Y Coordinate')
         plt.show()
 
+def load_tsp_file(file_path):
+    
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    nodes = []
+    node_section_started = False
+
+    for line in lines:
+        line = line.strip()
+        if line == "NODE_COORD_SECTION":
+            node_section_started = True
+            continue
+        if line == "EOF":
+            break
+        if node_section_started:
+            _, x, y = line.split()
+            nodes.append((float(x), float(y)))
+
+    return nodes
+
 if __name__ == '__main__':
-    berlin52_nodes = [
-        (565.0, 575.0), (25.0, 185.0), (345.0, 750.0), (945.0, 685.0),
-        (845.0, 655.0), (880.0, 660.0), (25.0, 230.0), (525.0, 1000.0),
-        (580.0, 1175.0), (650.0, 1130.0), (1605.0, 620.0), (1220.0, 580.0),
-        (1465.0, 200.0), (1530.0, 5.0), (845.0, 680.0), (725.0, 370.0),
-        (145.0, 665.0), (415.0, 635.0), (510.0, 875.0), (560.0, 365.0),
-        (300.0, 465.0), (520.0, 585.0), (480.0, 415.0), (835.0, 625.0),
-        (975.0, 580.0), (1215.0, 245.0), (1320.0, 315.0), (1250.0, 400.0),
-        (660.0, 180.0), (410.0, 250.0), (420.0, 555.0), (575.0, 665.0),
-        (1150.0, 1160.0), (700.0, 580.0), (685.0, 595.0), (685.0, 610.0),
-        (770.0, 610.0), (795.0, 645.0), (720.0, 635.0), (760.0, 650.0),
-        (475.0, 960.0), (95.0, 260.0), (875.0, 920.0), (700.0, 500.0),
-        (555.0, 815.0), (830.0, 485.0), (1170.0, 65.0), (830.0, 610.0),
-        (605.0, 625.0), (595.0, 360.0), (1340.0, 725.0), (1740.0, 245.0)
-    ]
+    tsp_file_path = './datasets/berlin52.tsp'
+    berlin52_nodes = load_tsp_file(tsp_file_path)
     random.seed(0)
 
     aco_solver = SolveTSPUsingACO(mode='ACS', colony_size=10, steps=100, nodes=berlin52_nodes)
